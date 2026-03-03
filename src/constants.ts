a381beeaa1901aa06f7d625b2e95115a56b7286a
fredev <federico@aternum.dev>
2026-03-03 06:18:35 +0100

    Create constants.ts
    feat: add constants

---
+++ src/constants.ts
@@ -0,0 +1,4 @@
+export const SCRIPTS_FOLDER = '/payload';
+export const HACK = SCRIPTS_FOLDER + '/hack.js';
+export const GROW = SCRIPTS_FOLDER + '/grow.js';
+export const WEAKEN = SCRIPTS_FOLDER + '/weaken.js';
